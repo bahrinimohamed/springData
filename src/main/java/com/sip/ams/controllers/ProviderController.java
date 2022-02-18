10c980dd13a1fc6c045bf16ce27bb77f7e0776e3
mohamed bahrini <mohamed.ba7rini.87@gmail.com>
2022-02-18 18:07:09 +0100

    data 3

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.sip.ams.entities.Provider;
 import com.sip.ams.repositories.ProviderRepository;
 
